293cc4d06c73de2759e703e925e7f7cd4084dd4f
Aaron <a.genevision14@gmail.com>
2024-05-01 00:02:04 -0500

    Completed development of 2d portfolio

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/gvdev7" target="_blank">Github</a>!`,
+  "wd-cert": `This is my WD cert. I hung it on the wall because I'm proud of it. It was a very technical certification but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube and Udemy.
+  You'll learn how I built this portfolio you're currently playing through!`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/GvDev7/Resume-CV/blob/master/McNeal%2C%20Aaron%20CV%20updated%2030-Apr-2024%20EN-GB.pdf" target="_blank">Check it out?</a>
+  Contact me at aemcneal7414@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+  Kaboom.js by watching the same tutorials I used at <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  library: `There are a lot of programming books on my shelves.
+  I've read a good bit of them starting off and then I found online resources. Youtube, FreeCodeCamp, Udemy, and so many more.`,
+  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+};
